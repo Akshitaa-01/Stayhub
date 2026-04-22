5b18cb0413b6b1f1643ac0caf511458bbffa2d7c
Akshita Bhardwaj <akshita.bhardwaj2006@gmail.com>
2026-04-22 20:20:36 +0530

    add index and show routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
+const path=require("path");
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine","ejs");
 
 let port=8080;
 
@@ -23,16 +27,33 @@ app.get("/",(req,res)=>{
   res.send("root is working ");
 });
 
-app.get("/testListing", async (req,res)=>{
-  const listing1= new Listing({
-    title:"my villa",
-    description:"dlbk9ulci",
-    price:1100,
-    location:"goa",
-    country:"india"
-  });
-
-  await listing1.save();
-  res.send("sample listing successfull");
-  console.log("db works");
+app.get("/listings",async (req,res)=>{
+  const allListings= await Listing.find();
+  res.render("./listings/home.ejs",{allListings});
 });
+
+app.get("/listings/:id", async (req,res)=>{
+  let Id=req.params.id;
+  const listing= await Listing.findById(Id);
+  res.render("./listings/show.ejs",{listing});
+});
+
+
+
+
+
+
+
+// app.get("/testListing", async (req,res)=>{
+//   const listing1= new Listing({
+//     title:"my villa",
+//     description:"dlbk9ulci",
+//     price:1100,
+//     location:"goa",
+//     country:"india"
+//   });
+
+//   await listing1.save();
+//   res.send("sample listing successfull");
+//   console.log("db works");
+// });
