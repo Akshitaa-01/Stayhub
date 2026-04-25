30c545f62bef684ec9670d485d2fd19394ac575a
Akshita Bhardwaj <akshita.bhardwaj2006@gmail.com>
2026-04-25 12:13:31 +0530

    Add edit and update route

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
-
+const methodOverride = require('method-override')
+ 
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 let port=8080;
 
@@ -29,11 +31,13 @@ app.get("/",(req,res)=>{
   res.send("root is working ");
 });
 
+//home route
 app.get("/listings",async (req,res)=>{
   const allListings= await Listing.find();
   res.render("./listings/home.ejs",{allListings});
 });
 
+//create route
 app.post("/listings", async (req,res)=>{
   const listing=req.body.listing;
   let listing1= await Listing.insertOne(listing);
@@ -41,12 +45,26 @@ app.post("/listings", async (req,res)=>{
   res.redirect("./listings");
 });
 
+//new route
 app.get("/listings/new",(req,res)=>{
   res.render("./listings/new.ejs");
 });
 
+//edit route
+app.get("/listings/:id/edit", async(req,res)=>{
+  let Id=req.params.id;
+  const listing= await Listing.findById(Id);
+  res.render("./listings/edit.ejs",{listing});
+});
 
+//update route 
+app.put("/listings/:id",async (req,res)=>{
+  let Id=req.params.id;
+  await Listing.findByIdAndUpdate(Id,{...req.body.listing});
+  res.redirect("/listings");
+});
 
+//show route
 app.get("/listings/:id", async (req,res)=>{
   let Id=req.params.id;
   const listing= await Listing.findById(Id);
