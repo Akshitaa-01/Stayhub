69276b04cb4630f8498da8e04233c38440d9d029
Akshita Bhardwaj <akshita.bhardwaj2006@gmail.com>
2026-05-06 05:20:04 +0530

    ui update complete

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
